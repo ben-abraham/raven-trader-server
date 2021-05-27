971a46d359001e6b4595a659c3b57faba5602957
Ben Abraham <ben.d.abraham@gmail.com>
2021-05-27 00:07:20 -0400

    Added helper class for parsing new listings. Added error messages to listing parsing.

--- raven-trader-server/Controllers/AssetListingController.cs
+++ raven-trader-server/Controllers/AssetListingController.cs
@@ -36,131 +36,32 @@ namespace raven_trader_server.Controllers
         [Route("list")]
         public JsonResult ListOrder([FromBody] ListingHex listing)
         {
-            bool valid = false;
-            ListingEntry entry = null;
+            bool valid = ListingEntry.TryParse(_rpc, listing, out var result, out var error, true);
 
-            if(Regex.IsMatch(listing.Hex, @"^[0-9a-fA-F]*$"))
-            {
-                var parse_test = _rpc.DecodeRawTransaction(listing.Hex);
-
-                if(parse_test != null)
-                {
-                    //Don't do anything special, if we sign and it says completed, that its properly validated to the owner of the UTXO
-                    var sign_test = _rpc.SignRawTransaction(listing.Hex);
-
-                    if(sign_test != null)
-                    {
-                        if (sign_test.Value<bool>("complete"))
-                        {
-                            valid = true;
-
-                            dynamic parsed_tx = parse_test;
-
-                            if (parsed_tx.vin[0]?.scriptSig?.asm?.ToString()?.Contains(Constants.SINGLE_ANYONECANPAY))
-                            {
-                                var utxo = $"{parsed_tx.vin[0].txid}|{parsed_tx.vin[0].vout}";
-                                var type = parsed_tx.vout[0]?.scriptPubKey?.type == Constants.VOUT_TYPE_TRANSFER_ASSET ? SwapType.Buy : SwapType.Sell;
-
-                                //var src_transaction = (dynamic)Utils.FullExternalTXDecode((string)parsed_tx.vin[0].txid);
-                                var src_transaction = (dynamic)_rpc.GetRawTransaction((string)parsed_tx.vin[0].txid);
-                                var src_vout = src_transaction.vout[(int)parsed_tx.vin[0].vout];
-
-                                var existing = _db.Find<ListingEntry>(utxo);
-                                if (existing != null)
-                                    entry = existing;
-                                else
-                                {
-                                    entry = new ListingEntry() { UTXO = utxo };
-                                    _db.Listings.Add(entry);
-                                }
-
-                                entry.B64SignedPartial = Convert.ToBase64String(Utils.StringToByteArray(listing.Hex));
-                                entry.OrderType = type;
-
-                                switch (entry.OrderType)
-                                {
-                                    case SwapType.Buy:
-                                        //For a buy order, the quantity is the amount being requested
-                                        entry.Quantity = parsed_tx.vout[0].scriptPubKey.asset.amount;
-                                        entry.AssetName = parsed_tx.vout[0].scriptPubKey.asset.name;
-                                        entry.UnitPrice = src_vout.amount / entry.Quantity;
-                                        break;
-                                    case SwapType.Sell:
-                                        entry.Quantity = src_vout.scriptPubKey.asset.amount;
-                                        entry.AssetName = src_vout.scriptPubKey.asset.name;
-                                        entry.UnitPrice = parsed_tx.vout[0].value / entry.Quantity;
-                                        break;
-                                }
-
-                                _db.SaveChanges();
-                            }
-                        }
-                    }
-                }
-            }
-
-            return new JsonResult (new { valid = valid, result = entry });
+            return new JsonResult(new ListingResult(valid, result, error));
         }
 
         [HttpPost]
         [Route("quickparse")]
         public JsonResult QuickParse([FromBody] ListingHex listing)
         {
-            bool valid = false;
+            bool valid = ListingEntry.TryParse(_rpc, listing, out var result, out var error, false);
 
-            if (Regex.IsMatch(listing.Hex, @"^[0-9a-fA-F]*$"))
-            {
-                var parse_test = _rpc.DecodeRawTransaction(listing.Hex);
-                if (parse_test != null)
-                {
-                    dynamic parsed_tx = parse_test;
-
-                    if (parsed_tx.vin[0]?.scriptSig?.asm?.ToString()?.Contains(Constants.SINGLE_ANYONECANPAY))
-                    {
-                        var utxo = $"{parsed_tx.vin[0].txid}|{parsed_tx.vin[0].vout}";
-                        var type = parsed_tx.vout[0]?.scriptPubKey?.type == Constants.VOUT_TYPE_TRANSFER_ASSET ? SwapType.Buy : SwapType.Sell;
-
-                        //var src_transaction = (dynamic)Utils.FullExternalTXDecode((string)parsed_tx.vin[0].txid);
-                        var src_transaction = (dynamic)_rpc.GetRawTransaction((string)parsed_tx.vin[0].txid);
-                        var src_vout = src_transaction.vout[(int)parsed_tx.vin[0].vout];
-
-                        ListingEntry entry = new ListingEntry();
-
-                        entry.B64SignedPartial = Convert.ToBase64String(Utils.StringToByteArray(listing.Hex));
-                        entry.OrderType = type;
-                        entry.UTXO = utxo;
-
-                        switch (entry.OrderType)
-                        {
-                            case SwapType.Buy:
-                                //For a buy order, the quantity is the amount being requested
-                                entry.Quantity = (double)parsed_tx.vout[0].scriptPubKey.asset.amount;
-                                entry.AssetName = parsed_tx.vout[0].scriptPubKey.asset.name.ToString();
-                                entry.UnitPrice = (entry.Quantity == 0) ? 0 : src_vout.value / entry.Quantity;
-                                break;
-                            case SwapType.Sell:
-                                entry.Quantity = (double)src_vout.scriptPubKey.asset.amount;
-                                entry.AssetName = src_vout.scriptPubKey.asset.name.ToString();
-                                entry.UnitPrice = (entry.Quantity == 0) ? 0 : ((double)parsed_tx.vout[0].value) / entry.Quantity;
-                                break;
-                        }
-
-                        return new JsonResult(new
-                        {
-                            valid = true,
-                            result = entry
-                        });
-                    }
-                }
-            }
-
-            return new JsonResult(new { valid = valid, result = (object)null });
+            return new JsonResult(new ListingResult(valid, result, error));
         }
     }
 
-    public class ListingHex
+    public class ListingResult
     {
-        public string Hex { get; set; }
-        public string Memo { get; set; }
+        public ListingResult(bool valid, ListingEntry result, string error)
+        {
+            Valid = valid;
+            Result = result;
+            Error = error;
+        }
+
+        public bool Valid { get; set; }
+        public ListingEntry Result { get; set; }
+        public string Error { get; set; }
     }
 }
--- raven-trader-server/Models/ListingEntry.cs
+++ raven-trader-server/Models/ListingEntry.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace raven_trader_server.Models
@@ -10,10 +12,96 @@ namespace raven_trader_server.Models
     {
         [Key]
         public string UTXO { get; set; }
+        public bool Active { get; set; }
+        public string Memo { get; set; }
         public string B64SignedPartial { get; set; }
         public SwapType OrderType { get; set; }
         public string AssetName { get; set; }
         public double Quantity { get; set; }
         public double UnitPrice { get; set; }
+
+
+        public static bool TryParse(RVN_RPC rpc, ListingHex listing, out ListingEntry Value, out string Error, bool ValidateSignature = true)
+        {
+            Value = null;
+            Error = null;
+
+            if (!Regex.IsMatch(listing.Hex, @"^[0-9a-fA-F]*$"))
+            {
+                Error = "Invalid input format.";
+                return false;
+            }
+
+            JObject decodedTX = null;
+            try
+            {
+                decodedTX = rpc.DecodeRawTransaction(listing.Hex);
+            }
+            catch { }
+
+            if (decodedTX == null)
+            {
+                Error = "Unable to decode transaction.";
+                return false;
+            }
+
+            if (ValidateSignature)
+            {
+                //Don't do anything special, if we sign and it says completed, that its properly validated to the owner of the UTXO
+                var sign_test = rpc.SignRawTransaction(listing.Hex);
+
+                if ((sign_test?.Value<bool>("complete") ?? false) == false)
+                {
+                    Error = "Signature is invalid";
+                    return false;
+                }
+            }
+
+            dynamic parsed_tx = decodedTX;
+
+            if (!parsed_tx.vin[0]?.scriptSig?.asm?.ToString()?.Contains(Constants.SINGLE_ANYONECANPAY))
+            {
+                Error = "Transaction is not signed with SINGLE|ANYONECANPAY. Not a valid swap.";
+            }
+
+            var utxo = $"{parsed_tx.vin[0].txid}|{parsed_tx.vin[0].vout}";
+            var type = parsed_tx.vout[0]?.scriptPubKey?.type == Constants.VOUT_TYPE_TRANSFER_ASSET ? SwapType.Buy : SwapType.Sell;
+
+            //var src_transaction = (dynamic)Utils.FullExternalTXDecode((string)parsed_tx.vin[0].txid);
+            var src_transaction = (dynamic)rpc.GetRawTransaction((string)parsed_tx.vin[0].txid);
+            var src_vout = src_transaction.vout[(int)parsed_tx.vin[0].vout];
+
+            Value = new ListingEntry()
+            {
+                UTXO = utxo,
+                B64SignedPartial = Convert.ToBase64String(Utils.StringToByteArray(listing.Hex)),
+                OrderType = type
+            };
+
+            switch (Value.OrderType)
+            {
+                case SwapType.Buy:
+                    //For a buy order, the quantity is the amount being requested
+                    Value.Quantity = parsed_tx.vout[0].scriptPubKey.asset.amount;
+                    Value.AssetName = parsed_tx.vout[0].scriptPubKey.asset.name;
+                    Value.UnitPrice = src_vout.amount / Value.Quantity;
+                    break;
+                case SwapType.Sell:
+                    Value.Quantity = src_vout.scriptPubKey.asset.amount;
+                    Value.AssetName = src_vout.scriptPubKey.asset.name;
+                    Value.UnitPrice = parsed_tx.vout[0].value / Value.Quantity;
+                    break;
+            }
+            return true;
+                    
+        }
+
     }
+
+    public class ListingHex
+    {
+        public string Hex { get; set; }
+        public string Memo { get; set; }
+    }
+
 }
