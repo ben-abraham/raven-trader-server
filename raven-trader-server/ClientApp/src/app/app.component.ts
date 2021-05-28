af256353fa3f748df1c5d8e5d940e35935f28601
Ben Abraham <ben.d.abraham@gmail.com>
2021-05-28 00:25:05 -0400

    Added preview and copy functionality for listed orders.

--- raven-trader-server/ClientApp/src/app/app.component.ts
+++ raven-trader-server/ClientApp/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
-import { DataService, DTOParseSignedPartial, SwapType } from './services/data.service';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { DataService, DTOParseSignedPartial, SwapType, DTOAssetListing } from './services/data.service';
 
 declare const $: any; //jQuery hack
 
@@ -12,8 +12,12 @@ export class AppComponent implements OnInit {
 
   title = 'app';
 
+  @ViewChild("swapHex", { static: true }) public swapHex: ElementRef;
+
   public signedPartialEntry = "";
   public signedPartialData: DTOParseSignedPartial = null;
+
+  public previewMode = false;
 
   public constructor(public dataService: DataService) {
   }
@@ -21,7 +25,46 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  showDetails(listing: DTOAssetListing): void {
+    $(this.swapHex.nativeElement).attr("readonly", "")
+    this.previewMode = true;
+    this.signedPartialEntry = this.base64ToHex(listing.b64SignedPartial);
+    this.signedPartialData = {
+      result: listing,
+      valid: true
+    };
+    $("#addSwapModal").modal("show");
+  }
+
+  delayedCopy(msDelay: number): void {
+    setTimeout(() => {
+      this.copySwapHex();
+    }, msDelay);
+  }
+
+  copySwapHex(): void {
+    console.log(this.swapHex.nativeElement.value)
+    this.swapHex.nativeElement.select();
+    document.execCommand('copy');
+    this.swapHex.nativeElement.setSelectionRange(0, 0);
+
+    $("#modalSwapFooter").attr("class", "text-success");
+    $("#modalSwapFooter").text("Copied!");
+    setTimeout(() => {
+      $("#modalSwapFooter").text("");
+    }, 1500);
+  }
+
+  showSubmitWindow(event: MouseEvent): void {
+    this.signedPartialEntry = ""
+    this.signedPartialData = null
+    this.previewMode = false;
+    $(this.swapHex.nativeElement).removeAttr("readonly")
+  }
+
   partialChanged(newValue: string): void {
+    if (this.previewMode) return;
+
     const newHex = this.signedPartialEntry = newValue;
     $("#modalSwapFooter").text("");
     if (/^[0-9a-fA-F]{32,}$/.exec(newHex)) {
@@ -53,6 +96,16 @@ export class AppComponent implements OnInit {
         }
       });
     }
+  }
+
+  base64ToHex(str): string {
+    const raw = atob(str);
+    let result = '';
+    for (let i = 0; i < raw.length; i++) {
+      const hex = raw.charCodeAt(i).toString(16);
+      result += (hex.length === 2 ? hex : '0' + hex);
+    }
+    return result.toUpperCase();
   }
 
 }
--- raven-trader-server/ClientApp/src/app/swaps/swaps.component.ts
+++ raven-trader-server/ClientApp/src/app/swaps/swaps.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { DataService, DTOSiteData, DTOSwap, DTOListingResults } from '../services/data.service';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { DataService, DTOSiteData, DTOSwap, DTOListingResults, DTOAssetListing } from '../services/data.service';
 import { Subscribable, Subject } from 'rxjs';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'swaps-component',
@@ -10,17 +11,21 @@ export class SwapListComponent implements OnInit {
 
   public results: DTOListingResults;
 
-  public constructor(public dataService: DataService) { }
+  public constructor(public app: AppComponent,
+    public dataService: DataService) { }
 
   ngOnInit(): void {
     this.loadData();
   }
 
   loadData(): void {
-    this.dataService.queryListings("RVNTEST1").subscribe(results => {
-      console.log("Got Swaps: ", results);
+    this.dataService.queryListings("").subscribe(results => {
       this.results = results;
     });
+  }
+
+  showDetails(order: MouseEvent, listing: DTOAssetListing): void {
+    this.app.showDetails(listing);
   }
 
 }
