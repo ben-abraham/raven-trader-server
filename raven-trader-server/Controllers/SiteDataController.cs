ae23b2a2690374d4b2d57408be410f42a17d24e8
Ben Abraham <ben.d.abraham@gmail.com>
2021-06-05 03:51:09 -0400

    Added enpoint for grouped listings. Very unoptimized currently.

--- raven-trader-server/Controllers/SiteDataController.cs
+++ raven-trader-server/Controllers/SiteDataController.cs
@@ -66,19 +66,76 @@ namespace raven_trader_server.Controllers
         }
 
         [HttpGet]
+        [Route("groupedlistings")]
+        public JsonResult GetGrouped(string assetName, string swapType, bool groupListing = false, int pageSize = 100, int offset = 0)
+        {
+            if (pageSize > Constants.MAX_PAGE_SIZE) pageSize = Constants.MAX_PAGE_SIZE;
+
+            //A bit expensive to look through all orders like this, but for now its okay :shrug:
+            var swapQuery = _db.Listings.AsQueryable().Where(l => l.Active);
+            if (!string.IsNullOrEmpty(assetName))
+                swapQuery = swapQuery.Where(s => s.InType.Contains(assetName) || s.OutType.Contains(assetName));
+            
+            var assetList = swapQuery.Select(l => l.OutType).Union(swapQuery.Select(l => l.InType))
+                .Where(t => t != "rvn")
+                .OrderBy(t => t)
+                .ToList();
+
+            int totalCount = assetList.Count();
+            var pageAssets = assetList.Skip(offset).Take(pageSize);
+
+            //Note: This is not SQL based and is therefore expesive AF
+            var assetResult = pageAssets.Select(pa =>
+            {
+                var buyOrders = _db.Listings
+                        .Where(l => l.OrderType == SwapType.Sell && l.InType.Contains(pa))
+                        .OrderBy(l => l.UnitPrice);
+                var sellOrders = _db.Listings
+                        .Where(l => l.OrderType == SwapType.Buy && l.OutType.Contains(pa))
+                        .OrderByDescending(l => l.UnitPrice);
+                var tradeOrders = _db.Listings
+                        .Where(l => l.OrderType == SwapType.Trade && (l.InType.Contains(pa) || l.OutType.Contains(pa)))
+                        .OrderByDescending(l => l.UnitPrice);
+
+                //Buy/Sell perspective is reversed as always here
+                return new
+                {
+                    Asset = pa,
+                    BuyOrders = buyOrders.Count(),
+                    BuyQuantity = buyOrders.Sum(o => o.OutQuantity),
+                    MinBuy = buyOrders.FirstOrDefault(),
+                    SellOrders = sellOrders.Count(),
+                    SellQuantity = sellOrders.Sum(o => o.InQuantity),
+                    MaxSell = sellOrders.FirstOrDefault(),
+                    TradeOrders = tradeOrders.Count(),
+                    TradeQuantity = tradeOrders.Sum(o => o.InType == pa ? o.InQuantity : o.OutQuantity),
+                    Trades = tradeOrders.FirstOrDefault()
+                };
+            });
+
+            return new JsonResult(new
+            {
+                offset = offset,
+                totalCount = totalCount,
+                assets = assetResult
+            });
+        }
+
+        [HttpGet]
         [Route("listings")]
-        public JsonResult GetListings(string assetName, string swapType, int pageSize = 100, int offset = 0)
+        public JsonResult GetListings(string assetName, string swapType, bool groupListing=false, int pageSize = 100, int offset = 0)
         {
             if (pageSize > Constants.MAX_PAGE_SIZE) pageSize = Constants.MAX_PAGE_SIZE;
 
             var swapQuery = _db.Listings.AsQueryable().Where(l => l.Active);
             if (!string.IsNullOrEmpty(assetName))
-                swapQuery = swapQuery.Where(s => s.InType == assetName || s.OutType == assetName);
+                swapQuery = swapQuery.Where(s => s.InType.Contains(assetName) || s.OutType.Contains(assetName));
             if (!string.IsNullOrEmpty(swapType) && Enum.TryParse<SwapType>(swapType, out var parsedType))
                 swapQuery = swapQuery.Where(s => s.OrderType == parsedType);
 
             var finalQuery = swapQuery.OrderByDescending(l => l.SubmitTime).Skip(offset).Take(pageSize);
             var totalCount = swapQuery.Count();
+
 
             return new JsonResult(new {
                 offset = offset,
@@ -96,7 +153,7 @@ namespace raven_trader_server.Controllers
             var swapQuery = _db.Swaps.AsQueryable();
 
             if (!string.IsNullOrEmpty(assetName))
-                swapQuery = swapQuery.Where(s => s.InType == assetName || s.OutType == assetName);
+                swapQuery = swapQuery.Where(s => s.InType.Contains(assetName) || s.OutType.Contains(assetName));
             if (!string.IsNullOrEmpty(swapType) && Enum.TryParse<SwapType>(swapType, out var parsedType))
                 swapQuery = swapQuery.Where(s => s.Type == parsedType);
 
